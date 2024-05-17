9dbe72fb3d1ac5e73bf0b1cebbac5fc1d250ca0c
nicolas <nicolasatoyebi@gmail.com>
2024-05-17 13:34:52 +0100

    second commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import styles from "./Nav.module.css"
+
+const Nav = () => {
+  return (
+    <div>
+        <h1 className={styles.header}>Nav</h1>
+        <h1 className="para">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia optio exercitationem illum pariatur ad nostrum dolores maxime aspernatur sunt repudiandae aliquam dicta in, ea dolor similique tempore quaerat, velit inventore!</h1>
+    </div>
+  )
+}
+
+export default Nav
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//to use bootstrap install botstrap through your cmd and import here
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/js/bootstrap.bundle.js"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
